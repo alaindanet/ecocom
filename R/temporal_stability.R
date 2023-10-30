b078896c6000eadaa690d9359cebeaf6449b5ddf
Alain Danet <alain.danet@mnhn.fr>
2023-10-30 12:37:06 +0000

    Add temporal stability metrics

---
+++ R/temporal_stability.R
@@ -0,0 +1,84 @@
+################################################################################
+#                        Functions to compute synchrony                        #
+################################################################################
+
+compute_sp_nb_from_com_mat <- function (com_mat = NULL) {
+
+  rowSums(com_mat != 0)
+
+}
+
+compute_avg_cv_sp <- function(biomass, variance) {
+  #Check that the species are in the same order in the vector:
+  stopifnot(names(biomass) == names(variance))
+
+  rel_biomass <- biomass / sum(biomass)
+  rel_sdt <- sqrt(variance) / biomass
+
+  cv_avg <- sum(rel_biomass * rel_sdt)
+  return(cv_avg)
+}
+
+compute_cv_com <- function(synchrony, cv_sp) {
+  sqrt(synchrony) * cv_sp
+}
+
+compute_synchrony <- function(cov_mat) {
+  com_var <- sum(cov_mat)
+  var_intra_sp <- sum(sqrt(diag(cov_mat)))
+  phi <- com_var / (var_intra_sp^2)
+  return(phi)
+}
+
+statistical_averaging_effect <- function(mat) {
+
+  var_sp <- apply(mat, 2, var)
+  sd_sp <- apply(mat, 2, sd)
+
+  statistical_averaging <- sum(sd_sp) / sqrt(sum(var_sp))
+  statistical_averaging_even <- sqrt(ncol(mat))
+  eveness <- statistical_averaging / statistical_averaging_even
+
+  c(
+    total = statistical_averaging,
+    even = statistical_averaging_even,
+    eveness = eveness
+  )
+}
+
+compensatory_effect <- function(mat) {
+  cov_mat <- cov(mat)
+  var_sp <- diag(cov_mat)
+  var_tot <- sum(cov_mat)
+  sqrt(sum(var_sp)) / sqrt(var_tot)
+}
+
+asynchrony <- function(mat) {
+
+  cov_mat <- cov(mat)
+  var_sp <- diag(cov_mat)
+  var_tot <- sum(cov_mat)
+
+  sum(sqrt(var_sp)) / sqrt(var_tot)
+}
+
+population_stability <- function(mat) {
+
+  bm_sp <- colMeans(mat)
+  bm_tot <- sum(bm_sp)
+
+  sd_sp <- apply(mat, 2, sd)
+
+  bm_tot / sum(sd_sp)
+}
+
+community_stability <- function(mat) {
+
+  bm_sp <- colMeans(mat)
+  bm_tot <- sum(bm_sp)
+
+  cov_mat <- cov(mat)
+  var_tot <- sum(cov_mat)
+
+  bm_tot / sqrt(var_tot)
+}
